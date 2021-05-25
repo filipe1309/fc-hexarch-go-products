c05a43278aa8d6d6eced2ff243756acd1cb6a946
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-05-24 23:13:08 -0300

    chore: implement & test Enable

--- application/product.go
+++ application/product.go
@@ -1,10 +1,12 @@
 package application
 
+import "errors"
+
 type ProductInterface interface {
 	IsValid() (bool, error)
 	Enable() error
 	Disable() error
-	GetId() string
+	GetID() string
 	GetName() string
 	GetStatus() string
 	GetPrice() float64
@@ -22,30 +24,35 @@ type Product struct {
 	Price  float64
 }
 
-func (p *Product) IsValid() (bool, error) {
+// func (p *Product) IsValid() (bool, error) {
 
-}
+// }
 
 func (p *Product) Enable() error {
+	if p.Price > 0 {
+		p.Status = ENABLED
+		return nil
+	}
 
+	return errors.New("the price must be greater than 0 to enable the product")
 }
 
-func (p *Product) Disable() error {
+// func (p *Product) Disable() error {
 
-}
+// }
 
-func (p *Product) GetId() string {
-
+func (p *Product) GetID() string {
+	return p.ID
 }
 
 func (p *Product) GetName() string {
-
+	return p.Name
 }
 
 func (p *Product) GetStatus() string {
-
+	return p.Status
 }
 
 func (p *Product) GetPrice() float64 {
-
+	return p.Price
 }
