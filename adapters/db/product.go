2c871047de654aaf31178d1fdca36dd8329371d4
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-06-01 21:33:05 -0300

    chore: init db adapter

---
+++ adapters/db/product.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"appproduct/application"
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type ProdyctDb struct {
+	db *sql.DB
+}
+
+func (p *ProdyctDb) Get(id string) (application.ProductInterface, error) {
+	var product application.Product
+	stmt, err := p.db.Prepare("SELECT  id, name. price, status FROM products WHERE id = ?")
+	if err != nil {
+		return nil, err
+	}
+	err = stmt.QueryRow(id).Scan(&product.ID, &product.Name, &product.Price, &product.Status)
+	if err != nil {
+		return nil, err
+	}
+
+	return &product, nil
+}
