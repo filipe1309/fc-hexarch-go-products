f3d71e8753e7b4ebd403cc01309baccdb0112265
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-06-05 17:03:39 -0300

    tests: save method of  product adapter

--- adapters/db/product_test.go
+++ adapters/db/product_test.go
@@ -2,6 +2,7 @@ package db_test
 
 import (
 	"appproduct/adapters/db"
+	"appproduct/application"
 	"database/sql"
 	"log"
 	"testing"
@@ -55,4 +56,26 @@ func TestProductDb_Get(t *testing.T) {
 	require.Equal(t, "Product Test", product.GetName())
 	require.Equal(t, 0.0, product.GetPrice())
 	require.Equal(t, "disabled", product.GetStatus())
+}
+
+func TestProductDb_Save(t *testing.T) {
+	setUp()
+	defer Db.Close()
+	productDb := db.NewProductDb(Db)
+
+	product := application.NewProduct()
+	product.Name = "Product Test"
+	product.Price = 25
+
+	productResult, err := productDb.Save(product)
+	require.Nil(t, err)
+	require.Equal(t, product.Name, productResult.GetName())
+	require.Equal(t, product.Price, productResult.GetPrice())
+	require.Equal(t, product.Status, productResult.GetStatus())
+
+	product.Status = "enabled"
+	require.Nil(t, err)
+	require.Equal(t, product.Name, productResult.GetName())
+	require.Equal(t, product.Price, productResult.GetPrice())
+	require.Equal(t, product.Status, productResult.GetStatus())
 }
